1d80281e78a90fb69e6b7817478780688af7e846
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-03-14 20:27:22 +1000

    tset commit

---
+++ source/repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Задание 10.2.3");
+
+            Console.Write("Введите число N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            while (N >= 1)
+            {
+                if (N % 2 != 0)
+                {
+                    count++;
+                }
+                N = N / 2;
+            }
+
+            Console.Write("Разрядность числа N: ");
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
+
